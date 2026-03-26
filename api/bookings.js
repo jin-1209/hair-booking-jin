4b8266f91a96638b0cd76bf20920b070d790b49e
JIN YOSHINAGA <jinyoshinaga@192.168.1.5>
2026-03-26 10:17:58 +0800

    API version header追加 + デプロイ確認

--- api/bookings.js
+++ api/bookings.js
@@ -73,6 +73,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('X-API-Version', 'v3-fallback');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
